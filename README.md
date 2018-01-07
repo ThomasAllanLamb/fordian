820b1db0c6bcedc213fa6b80aa8a41989c7c161c
Thomas Allan Lamb <lambyte@lambyte.com>
2018-01-07 13:45:30 -0800

    uses docker(lnp and php-fpm)

---
+++ README.md
@@ -0,0 +1,10 @@
+```bash
+docker-compose up
+```
+
+in hosts:
+```
+127.0.0.1 docker-lanp.local
+```
+
+visit [http://docker-lanp.local:8080]
---
+++ code/public/index.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(E_ALL);
+phpinfo();
+echo "what";
+/*$pdo = new \PDO(
+    'mysql:host=db;dbname=demoName',
+    'demoUser',
+    'demoPass'
+);*/
