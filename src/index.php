cfb75e604b523f535cd86cea144fe8e03de31699
Thomas Allan Lamb <lambyte@lambyte.com>
2018-01-07 13:23:51 -0800

    Discovery

--- src/index.php
+++ src/index.php
@@ -1,25 +1,24 @@
-<?php
-  
-  # Fill our vars and run on cli
-  # $ php -f db-connect-test.php
-  $dbname = 'fordian';
-  $dbuser = 'root';
-  $dbpass = '';
-  $dbhost = 'db';
-  $connect = mysql_connect("$dbhost", $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  mysql_select_db($dbname) or die("Could not open the db '$dbname'");
-  $test_query = "SHOW TABLES FROM $dbname";
-  $result = mysql_query($test_query);
-  $tblCnt = 0;
-  while($tbl = mysql_fetch_array($result)) {
-    $tblCnt++;
-    #echo $tbl[0]."<br />\n";
-  
-  if (!$tblCnt) {
-    echo "There are no tables<br />\n";
-  } else {
-    echo "There are $tblCnt tables<br />\n";
-  }
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
 
-?>
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="js/jquery-3.2.1.js"></script>
+    </head>
+    <body>
+        <h1>Fordian Todo</h1>
+        <div>“Nothing is particularly hard if you divide it into small jobs” &emdash; Henry Ford</div>
+
+        <div id="todo"></div>
+
+        <script src="js/main.js"></script>
+    </body>
+</html>
